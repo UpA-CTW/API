33b6250f16dbca84d9062b7ebebd5007276b5246
Aluno Manhã <alunomanha@Altars-Mac-mini-3.local>
2020-01-04 20:50:18 +0000

    question queries corrected

--- projectInterview/src/main/java/models/Question.java
+++ projectInterview/src/main/java/models/Question.java
@@ -9,7 +9,7 @@ import javax.persistence.OneToMany;
 
 @Entity
 @NamedQuery(name=Question.GET_ALL_QUESTIONS_QUERY_NAME, query="SELECT q FROM Question q")
-@NamedQuery(name=Question.GET_QUESTION_BY_VALUE_QUERY_NAME, query="SELECT q FROM Question q WHERE q.attributes.value  = :value")
+@NamedQuery(name=Question.GET_QUESTION_BY_VALUE_QUERY_NAME, query="SELECT q FROM Question q WHERE q  = :value")
 
 public class Question extends GenericEntity {
 

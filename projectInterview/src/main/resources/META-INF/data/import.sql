e0a43aee5c8f663395ad0aa3ea794ae48d495495
Aluno Manhã <alunomanha@Altars-Mac-mini-3.local>
2020-01-06 10:37:58 +0000

    server initializes only with admin

--- projectInterview/src/main/resources/META-INF/data/import.sql
+++ projectInterview/src/main/resources/META-INF/data/import.sql
@@ -1,9 +1,1 @@
-INSERT INTO User(name, email, hashcode, salt, role) VALUES ('manel', 'm@g.com', 'MpJccCNc', 'JEPxo6+onnzDx+4AABLSLLrzDRNgXqmyo+DU39CBXVLtQhlhLfqlLQ6/F867AQyMZRQ=', 'Admin')
 INSERT INTO User(name, email, hashcode, salt, role) VALUES ('admin', 'admin', 'MpJccCNc', 'JEPxo6+onnzDx+4AABLSLLrzDRNgXqmyo+DU39CBXVLtQhlhLfqlLQ6/F867AQyMZRQ=', 'Admin')
-
-INSERT INTO Attribute(category) VALUES ('Technology')
-INSERT INTO Attribute(category) VALUES ('Role')
-
-INSERT INTO AttributeValue(value) VALUES ('Java')
-INSERT INTO AttributeValue(value) VALUES ('Java Script')
-INSERT INTO AttributeValue(value) VALUES ('.net')
